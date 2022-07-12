f0da5bd82a5ee60fae561055b56f5bc5cf0bb49b
son <sonvnmse150320@fpt.edu.vn>
2022-07-12 14:42:15 +0700

    30

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,7 +6,35 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+class Main extends Component {
+
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
+      };
+    }
+    const HomePage = () => {
+        return(
+            <Home 
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
+        );
+      }
+  
+<Route exact path='/contactus' component={Contact} />
 class Main extends Component {
 
   constructor(props) {
