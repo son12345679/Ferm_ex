8a6d4f7b872b792bfc1781420117245ae3b8f5c8
son <sonvnmse150320@fpt.edu.vn>
2022-07-12 15:08:59 +0700

    34

--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -1,5 +1,8 @@
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label } from 'reactstrap';
 
 class Header extends Component {
     constructor(props) {
@@ -16,6 +19,39 @@ class Header extends Component {
           isNavOpen: !this.state.isNavOpen
         });
       }
+<Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                            </Nav>
+                        <Form onSubmit={this.handleLogin}>
+                        <FormGroup>
+                            <Label htmlFor="username">Username</Label>
+                            <Input type="text" id="username" name="username"
+                                innerRef={(input) => this.username = input} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="password">Password</Label>
+                            <Input type="password" id="password" name="password"
+                                innerRef={(input) => this.password = input}  />
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" name="remember"
+                                innerRef={(input) => this.remember = input}  />
+                                Remember me
+                            </Label>
+                        </FormGroup>
+                        <Button type="submit" value="submit" color="primary">Login</Button>
+                    </Form>
+     this.handleLogin = this.handleLogin.bind(this);
+     handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
+    }
 
     render() {
         return(
@@ -53,6 +89,22 @@ class Header extends Component {
                     </div>
                 </Jumbotron>
             </div>
-        );
+        );        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);      
+        toggleModal() {
+            this.setState({
+              isModalOpen: !this.state.isModalOpen
+            });
+          }
+          <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+          
+          </ModalBody>
+      </Modal>
+
     }
 }
