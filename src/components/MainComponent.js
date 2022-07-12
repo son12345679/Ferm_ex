ae57979539109599c41352fe8f5ecdbcd5b33587
son <sonvnmse150320@fpt.edu.vn>
2022-07-12 14:44:18 +0700

    31

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,7 +13,13 @@ import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
-
+    const DishWithId = ({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        );
+      };
+  
     constructor(props) {
       super(props);
   
@@ -73,7 +79,7 @@ class Main extends Component {
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
       </div>
     );
-  }
+  }              <Route path='/menu/:dishId' component={DishWithId} />
 }
 <Switch>
 <Route path='/home' component={HomePage} />
