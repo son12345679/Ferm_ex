31288be8fece0658b1b2a90683076b3fd82b59b7
son <sonvnmse150320@fpt.edu.vn>
2022-07-12 13:57:29 +0700

    24

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import DishDetail from "./components/DishDetailComponent.js";
 import { DISHES } from './shared/dishes.js';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
@@ -16,7 +17,7 @@ class App extends Component {
     );
   }
 }
-
+<Menu />
 
 
 export default App;
